6609960b89c66d6053a115756da38389b12665fe
GameBuilder101 <56798450+GameBuilder101@users.noreply.github.com>
2023-05-16 21:11:30 -0400

    Map sounds and resources

--- source/gbc/sound/MusicManager.hx
+++ source/gbc/sound/MusicManager.hx
@@ -8,17 +8,13 @@ class MusicManager
 	/** Transitions the music by fading in/out. **/
 	public static function transition(music:AssetMusic, duration:Float)
 	{
+		createMusic();
 		if (duration <= 0.0)
 		{
 			music.play();
 			return;
 		}
 
-		if (FlxG.sound.music == null) // For some reason FlxG must have playMusic called before FlxG.sound.music gets created
-		{
-			music.play();
-			FlxG.sound.music.volume = 0.0;
-		}
 		FlxG.sound.music.fadeOut(duration / 2.0, 0.0, function(tween:FlxTween)
 		{
 			music.play();
@@ -28,6 +24,18 @@ class MusicManager
 
 	public static function fadeOut(duration:Float)
 	{
-		FlxG.sound.music.fadeOut(duration);
+		createMusic();
+		if (duration <= 0.0)
+			FlxG.sound.music.volume = 0.0;
+		else
+			FlxG.sound.music.fadeOut(duration);
+	}
+
+	/** For some reason FlxG must have playMusic called before FlxG.sound.music gets created. **/
+	static function createMusic()
+	{
+		if (FlxG.sound.music != null)
+			return;
+		FlxG.sound.playMusic(null);
 	}
 }
--- source/ui/map_hud/MapHud.hx
+++ source/ui/map_hud/MapHud.hx
@@ -3,10 +3,14 @@ package ui.map_hud;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.tweens.FlxTween;
+import gbc.sound.AssetSound;
+import gbc.sound.AssetSoundRegistry;
 
 class MapHud extends FlxTypedGroup<FlxSprite>
 {
 	public var levelPanel(default, null):LevelPanel;
+
+	var levelPanelAppearSound:AssetSound;
 
 	public function new()
 	{
@@ -17,6 +21,7 @@ class MapHud extends FlxTypedGroup<FlxSprite>
 		levelPanel.setPosition(levelPanel.x, 0.0);
 		levelPanel.visible = false; // The level panel starts hidden by default
 		add(levelPanel);
+		levelPanelAppearSound = AssetSoundRegistry.getAsset("ui/map_hud/sounds/level_panel_appear");
 	}
 
 	/** Plays an animation to show the level panel. **/
@@ -25,6 +30,8 @@ class MapHud extends FlxTypedGroup<FlxSprite>
 		levelPanel.visible = true;
 		FlxTween.cancelTweensOf(levelPanel);
 		FlxTween.linearMotion(levelPanel, levelPanel.x, -levelPanel.height, levelPanel.x, 0.0, 0.25);
+
+		levelPanelAppearSound.play();
 	}
 
 	/** Plays an animation to hide the level panel. **/
